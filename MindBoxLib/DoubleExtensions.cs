63e2f3ab58c91bf3f8e6138220e422d9494412a7
16Shadows <egor.0.00@mail.ru>
2024-07-18 15:36:17 +0500

    chore: Расширено сравнение double.
    Добавлен метод для приблизительного сравнения double с усечением до n-го разряда после запятой.

--- MindBoxLib/DoubleExtensions.cs
+++ MindBoxLib/DoubleExtensions.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MindBoxLib
+﻿namespace MindBoxLib
 {
-	internal static class DoubleExtensions
+	public static class DoubleExtensions
 	{
 		public static bool AlmostEquals(this double a, double b)
 		{
 			double epsilon = Math.Max(Math.Abs(a), Math.Abs(b)) * 1E-15;
 			return Math.Abs(a - b) < epsilon;
+		}
+
+		public static bool AlmostEquals(this double a, double b, int digits)
+		{
+			return AlmostEquals(Floor(a, digits), Floor(b, digits));
+		}
+
+		private static double Floor(double a, int digits)
+		{
+			double correctABy = a - Math.Round(a, digits);
+			return a - (correctABy >= 0 ? correctABy : Math.Pow(10, -digits) + correctABy);
 		}
 	}
 }
